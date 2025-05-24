8aced9ba959066a836272714c33158c532e4bd84
ashigarupen <droids_jab.8n@icloud.com>
2025-05-24 23:37:57 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,14 @@
+//　体重の変数の宣言
+let weight;
+
+// 体重の値の代入
+weight = 68;
+
+// 身長の変数の宣言
+let height = 1.7;
+
+// BMIの計算結果を変数に代入する
+let bmi = weight / (height * height);
+
+// BMIをコンソールに出力する
+console.log(bmi);
