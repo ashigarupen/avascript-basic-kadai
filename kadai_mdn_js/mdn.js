08ae7a373ca2ae59ed8a902f5e9fa96728a375d8
ashigarupen <droids_jab.8n@icloud.com>
2025-06-11 09:03:10 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,13 @@
+const today = new Date();
+
+ // 年の取得
+const year = today.getFullYear();
+
+// 月の取得、月は0から始まるため+1する
+const month = today.getMonth() + 1; 
+
+// 日付（日）の取得
+const day = today.getDate(); 
+
+//今日の年、月、日を表示する
+console.log(year + '年' + month + '月' + day + '日');
