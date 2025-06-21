dd0616c1436fd362cef7d38cccb889b480987f23
ashigarupen <droids_jab.8n@icloud.com>
2025-06-21 13:03:38 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+//HTML要素をidで取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+//textというidを取得し、定数に代入する
+const text = document.getElementById('text');
+
+//HML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+  text.textContent = 'ボタンをクリックしました';
+});
